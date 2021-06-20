0bb05b66e34dd5611ae9f5f8451faee450128509
Nodir <nodirxakimov@yandex.ru>
2021-06-21 00:50:26 +0500

    Nothing changed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,6 +40,7 @@ class HomeController extends Controller
     public function ways(Request $request)
     {
         // return $request;
+        $price = null;
         $validate = $request->validate([
                 'regionFrom'    => 'required',
                 'regionTo'      => 'required',
